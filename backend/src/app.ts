34c00cde5dc06a142499418c9f612828479d1948
DenisDev <imperor.oktov@gmail.com>
2026-01-18 14:10:33 +0200

    add express

---
+++ backend/src/app.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.get('/health', (req, res) => {
+    res.status(200).json({ status: 'OK'});
+});
+
+export default app;
