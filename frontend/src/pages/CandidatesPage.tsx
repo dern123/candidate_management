f4d7e45d5003f837d4b59467380aa9b68c4e6527
DenisDev <imperor.oktov@gmail.com>
2026-01-18 20:09:44 +0200

    update App

---
+++ frontend/src/pages/CandidatesPage.tsx
@@ -0,0 +1,13 @@
+
+
+export function CandidatePage() {
+ 
+
+    return (
+        <div>
+            <div className="mx-auto max-w-6xl p-4 md:p-6">
+             
+            </div>
+       </div> 
+    );
+}
